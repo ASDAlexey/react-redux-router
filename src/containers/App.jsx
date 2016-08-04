2e45a6a8ae3021b0ae33bd5c0aa69d59616f2fac
Alexey Popov <alexey.popov@umbrella-web.com>
2016-08-04 17:20:23 +0300

    activate index route

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,6 +7,7 @@ export default class App extends Component {
         return (
             <div className="container">
                 <ul className="nav nav-pills">
+                    <li><NavLink onlyActiveOnIndex={true} to="/">Главная</NavLink></li>
                     <li><NavLink to="/admin">Админка</NavLink></li>
                     <li><NavLink to="/list">Список жанров</NavLink></li>
                 </ul>
