5aec89d0c935f7d92441e0814ed1ff74443784a5
Alexey Popov <alexey.popov@umbrella-web.com>
2016-08-04 17:33:31 +0300

    router push

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 export default class Home extends Component {
+    handleSubmit(e) {
+        e.preventDefault();
+        const value = e.target.elements[0].value.toLowerCase();
+        this.context.router.push(`/genre/${value}`)
+    }
+
     render() {
         return (
-            <div className="row">
-                <div className="col-md-12">Раздел /</div>
+            <div className='row'>
+                <div className='col-md-12'>Раздел /</div>
+                <form className='col-md-4' onSubmit={::this.handleSubmit}>
+                    <input type='text' placeholder='genreName' />
+                    <button type='submit'>Перейти</button>
+                </form>
             </div>
         );
     }
 }
+
+Home.contextTypes = {
+    router: PropTypes.object.isRequired
+};
