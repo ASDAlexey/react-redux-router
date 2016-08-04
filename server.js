782a17282a39fab7eece1c947d3c08e91cbd69bf
Alexey Popov <alexey.popov@umbrella-web.com>
2016-08-04 15:58:56 +0300

    fix server

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-var webpack = require('webpack');
-var WebPackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const webpack = require('webpack');
+const WebPackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
 new WebPackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(8080, 'localhost', function(err, result){
-  if(err){
-    return console.log(err);
-  }
-
-  console.log('Listening on localhost:8080')
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+}).listen(8080, 'localhost', (err, result) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Listening on localhost:8080');
 });
